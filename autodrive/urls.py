1f02db4582d3af831b9bb0565e29dd044c121ea8
Alnaqeeb Eissa <alnaqeebeissa@gmail.com>
2025-10-19 14:08:26 +0500

    Migrate static landing page to Django storefront

---
+++ autodrive/urls.py
@@ -0,0 +1,10 @@
+"""URL configuration for the AutoDrive project."""
+from __future__ import annotations
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("market.urls")),
+]
